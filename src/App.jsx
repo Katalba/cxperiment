fc508914795088a91de1978992d5270a7a634fe9
guille <guillermoneculqueo@gmail.com>
2023-06-10 05:34:06 -0300

    config estilos personalizados de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className='text-center'>CXperiment</h1>
+    <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
+      <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
+      <button className='botonVerde rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+      <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+    </div>
   )
 }
 
