c99598deeb844b606708272ea4b9eb542ac007ab
guille <guillermoneculqueo@gmail.com>
2023-06-11 03:27:36 -0300

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
-      <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
-      <button className='botonVerde rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
-      <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
-    </div>
+    <>
+      <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
+        <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
+        <button className='botonVerde rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+        <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+      </div>
+      <Routes>
+        <Route path='/' element = {<h1>Home</h1>}></Route> 
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
+        <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
+        <button className='botonVerde rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+        <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+    
+    
+    </div>
+  )
+}
+
+export default Home
