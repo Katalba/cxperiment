608e0b3d30901e98624685d94fbe38523b31fb8e
guille <guillermoneculqueo@gmail.com>
2023-06-11 05:33:49 -0300

    se creo el ruteo base + error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
+import BotonBase from '../components/BotonBase'
+
 const Home = () => {
   const algo = 'algo'
   console.log(algo)
   return (
     <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
       <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
-      <button className='botonVerde rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
-      <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
+      <BotonBase name='enviar1' clase='botonVerde' />
+      <BotonBase name='enviar2' clase='botonWhite' />
     </div>
   )
 }
