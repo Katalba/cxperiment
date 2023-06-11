2b46912eed0c2c5a5d64e4f78c4ce794e4c0ccf4
guille <guillermoneculqueo@gmail.com>
2023-06-11 04:04:26 -0300

    prueba

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+
+const Error404 = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-purpleBal">
+        <div id="error-page" className="w-auto h-auto flex flex-col items-center justify-center gap-5">
+          <h1 className="lg:text-6xl font-bold text-2xl font-parrafo text-whiteWi">Oops!</h1>
+          <p className="text-xl text-whiteWi font-parrafo">
+            Ruta no encontrada, disculpe
+          </p>
+          <p className="text-3xl text-whiteWi font-parrafo">
+            error 404
+          </p>
+          <div className="mt-4">
+            <Link
+              to={'/'}
+              className="botonVerde px-5 py-2 font-parrafo text-sm rounded-md transition duration-300 ease-in-out"
+            >
+              vuelve al home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default Error404
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 
 const Home = () => {
+  const  algo = 'algo';
+  console.log(algo); 
   return (
     <div className='w-full h-screen flex flex-col bg-purple-300 justify-center items-center gap-5'>
         <h1 className=' text-4xl text-white font-parrafo font-[700]'>CXperiment</h1>
@@ -7,6 +9,8 @@ const Home = () => {
         <button className='botonWhite rounded-md transition-all duration-300 ease-in-out'>CXperiment</button>
     </div>
   )
+
+
 }
 
 export default Home
