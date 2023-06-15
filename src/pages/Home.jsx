8c5d224fd8242dee38b264de061648343446d908
guille <guillermoneculqueo@gmail.com>
2023-06-15 20:51:39 -0300

    me piso la modificacion del gap general

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
 import CardPrecentacion from '../components/CardPrecentacion'
 import Nav from '../components/Nav'
 import HomeBanner from '../components/HomeBanner'
-import FlipCardComp from '../components/FlipCardComp'
+import FlipCard from '../components/FlipCard'
+
 import Contacto from '../components/Contacto'
 import CardListContenido from '../components/CardListContenido'
 import Titulo from '../components/Titulo'
+import FlipCardComp from '../components/FlipCardComp'
 
 const Home = () => {
   return (
     <div className='w-full min-h-screen h-auto flex flex-col bg-white justify-start items-center'>
       <Nav />
       <HomeBanner />
-      <CardPrecentacion />
-      <Titulo titulo='Servicios' />
-      <FlipCardComp />
-      <Titulo titulo='Descubrí los beneficios de trabajar juntos' />
-      <CardListContenido />
-      <Contacto />
+      <section className='w-full h-auto flex flex-col bg-white justify-start items-center gap-10 lg:gap-20'>
+        <CardPrecentacion />
+        <Titulo titulo='Servicios' />
+        <FlipCardComp />
+        <Titulo titulo='Descubrí los beneficios de trabajar juntos' />
+        <CardListContenido />
+        <Contacto />
+      </section>
     </div>
   )
 }
