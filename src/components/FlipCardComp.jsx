18f9e8f048fa77637444d62f84296c46c3829d51
guille <guillermoneculqueo@gmail.com>
2023-06-16 01:14:28 -0300

    se añade nuevamente el gap

--- src/components/FlipCardComp.jsx
+++ src/components/FlipCardComp.jsx
@@ -2,8 +2,8 @@ import FlipCard from './FlipCard'
 
 const FlipCardComp = () => {
   return (
-    // <section className='w-[1200px] h-[441px] flex'>
-    <section className='w-full h-auto flex flex-col max-w-[1650px] lg:flex-row justify-center items-center overflow-hidden py-10 gap-5 lg:gap-5'>
+  // <section className='w-full h-auto flex flex-col max-w-[1650px] lg:flex-row justify-center items-center overflow-hidden py-10 gap-5 lg:gap-5'>
+    <section className='w-[1200px] h-[441px] flex'>
       <FlipCard
         bgColor='purpleBal'
         textFront='Conociendo realmente a tu cliente'
