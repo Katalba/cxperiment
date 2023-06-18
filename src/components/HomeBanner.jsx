1162e6220c16dacec144496d618d7cbb7507fe01
guille <guillermoneculqueo@gmail.com>
2023-06-18 19:36:16 -0300

    arreglando nuevamente el ref

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -3,7 +3,7 @@ import { IoArrowForwardSharp } from 'react-icons/io5'
 
 const HomeBanner = () => {
   return (
-    <div className='homeImage w-full h-[161px] md:h-[380px] lg:h-[500px] mt-[90px] bg-cover bg-center relative -z-0 mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+    <div id='home' className='homeImage w-full h-[161px] md:h-[380px] lg:h-[500px] mt-[90px] bg-cover bg-center relative -z-0 mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
       <div className='containerTitle md:w-1/2  lg:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start lg:items-start md:text-left lg:text-left mb-16 md:mb-0 lg:mb-0 items-center text-center text-purpleBal'>
         <h1 className='title-font sm:text-4xl text-3xl mb-8 font-medium'> Convertí a tus clientes en tus fans</h1>
         <p className='text-justify mb-8 leading-relaxed'>Mejoramos la experiencia de tus clientes y tu reputación de marca, identificamos oportunidades, implementamos soluciones efectivas, y ayudamos a impulsar tu crecimiento en el mercado.</p>
@@ -19,4 +19,3 @@ const HomeBanner = () => {
 }
 
 export default HomeBanner
-
