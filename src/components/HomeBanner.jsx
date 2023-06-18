13bb4a3bc7f53ea490283484b30b1bf4762bea47
katalba <katherine.albanicg@gmail.com>
2023-06-18 02:54:27 -0300

    HeroBannerPrueba

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,33 +1,21 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
 import BotonBase from './BotonBase'
 import { IoArrowForwardSharp } from 'react-icons/io5'
 
 const HomeBanner = () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      className='mySwiper w-full h-[450px] sm:h-[500px] md:h-[550px] lg:h-[650px] xl:h-[800px] sm:flex-row mt-[91px]'
-    >
-      <SwiperSlide>
-        <div className='w-full sm:h-[161px] md:h-[262px] lg:h-[363px] xl:h-[509px] flex'>
-          <div className='homeImage  sm:flex-row h-full' />
-          <div className='homeTitle absolute top-[50%] sm:left-[1rem] lg:left-[3rem] sm: w-1/2 lg:w-2/5'>
-            <h1 className='font-medium sm:text-[34px] md:text-[50px] lg:text-[56px]'>Convertí a tus clientes en tus fans</h1>
-            <p className='text-justify hidden sm:block'>Mejoramos la experiencia de tus clientes y tu reputación de marca, identificamos oportunidades, implementamos soluciones efectivas, y ayudamos a impulsar tu crecimiento en el mercado.</p>
-            <div className='buttons sm:space-x-4'>
-              <BotonBase name='Ver servicio' clase='botonVerde'>
-                <IoArrowForwardSharp />
-              </BotonBase>
-              <BotonBase name='Reservar consultoría' clase='botonWhite' />
-            </div>
-          </div>
+    <div className='homeImage w-full h-[600px] z-0 bg-cover bg-center relative'>
+      <div className='containerTitle space-y-6 text-purpleBal absolute top-[30%] sm:left-[1rem] lg:left-[3rem] sm:w-1/2 lg:w-2/5'>
+        <h1 className='sm:text-[34px] md:text-[50px] lg:text-[56px] landing-6'> Convertí a tus clientes en tus fans</h1>
+        <p className='text-justify'>Mejoramos la experiencia de tus clientes y tu reputación de marca, identificamos oportunidades, implementamos soluciones efectivas, y ayudamos a impulsar tu crecimiento en el mercado.</p>
+        <div className='containerButtons flex space-x-2'>
+          <BotonBase name='Ver servicio' clase='botonWhite'>
+            <IoArrowForwardSharp />
+          </BotonBase>
+          <BotonBase name='Asesoría inicial sin cargo' clase='botonVerde' />
         </div>
+      </div>
 
-      </SwiperSlide>
-
-    </Swiper>
+    </div>
   )
 }
 
