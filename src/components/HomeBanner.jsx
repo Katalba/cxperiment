ef68b74fd13e65c58e02ba7957eb5d791bcac5fb
katalba <katherine.albanicg@gmail.com>
2023-06-18 16:22:39 -0300

    faltaAcomadarTexto

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -3,10 +3,10 @@ import { IoArrowForwardSharp } from 'react-icons/io5'
 
 const HomeBanner = () => {
   return (
-    <div className='homeImage w-full h-[600px] z-0 bg-cover bg-center relative'>
-      <div className='containerTitle space-y-6 text-purpleBal absolute top-[30%] sm:left-[1rem] lg:left-[3rem] sm:w-1/2 lg:w-2/5'>
-        <h1 className='sm:text-[34px] md:text-[50px] lg:text-[56px] landing-6'> Convertí a tus clientes en tus fans</h1>
-        <p className='text-justify'>Mejoramos la experiencia de tus clientes y tu reputación de marca, identificamos oportunidades, implementamos soluciones efectivas, y ayudamos a impulsar tu crecimiento en el mercado.</p>
+    <div className='homeImage w-full h-[161px] md:h-[380px] lg:h-[500px] mt-[90px] bg-cover bg-center relative -z-0 mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+      <div className='containerTitle md:w-1/2  lg:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start lg:items-start md:text-left lg:text-left mb-16 md:mb-0 lg:mb-0 items-center text-center text-purpleBal'>
+        <h1 className='title-font sm:text-4xl text-3xl mb-8 font-medium'> Convertí a tus clientes en tus fans</h1>
+        <p className='text-justify mb-8 leading-relaxed'>Mejoramos la experiencia de tus clientes y tu reputación de marca, identificamos oportunidades, implementamos soluciones efectivas, y ayudamos a impulsar tu crecimiento en el mercado.</p>
         <div className='containerButtons flex space-x-2'>
           <BotonBase name='Ver servicio' clase='botonWhite'>
             <IoArrowForwardSharp />
@@ -14,7 +14,6 @@ const HomeBanner = () => {
           <BotonBase name='Asesoría inicial sin cargo' clase='botonVerde' />
         </div>
       </div>
-
     </div>
   )
 }
