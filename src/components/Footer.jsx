1b5d056e4a969192fb40b1e29d221f4afd2ac296
katalba <katherine.albanicg@gmail.com>
2023-06-18 18:10:21 -0300

    footerDone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-purpleBal text-whiteWi text-[18px] lg:h-[382px] md:h-[257px] sm:h-[347px] '>
-      <div className='flex items-center p-[5%]'>
-        <div className='logo mr-[5%] lg:w-[247px] lg:h-[31.47px]'>
+    <div className='w-full bg-purpleBal text-whiteWi text-[18px] '>
+      <div className='items-start p-[5%] pb-[8rem] pt-[20px] mx-auto flex md:items-center md:flex-row md:flex-nowrap flex-wrap flex-col space-y-10'>
+        <div className='logo mr-[5%] w-[70%] h-[10%] md:w-[30%] md:h-[3%]'>
           <img className='w-full h-full' src='https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/logoWhite.png?alt=media&token=e025e897-8318-46c4-b001-3ad03397f262' alt='logo' />
         </div>
-        <div className='contact mr-[30%]'>
+        <div className='contact w-[45%] flex-shrink-0 md:mx-0 text-left'>
           <p>Jesica Bilotti</p>
           <p>WhatsApp +54 11 4156-8854</p>
           <p>info.cxperiment@gmail.com</p>
         </div>
-        <div className='redes'>
+        <div className='redes md:mx-0'>
           <h3>Redes Sociales</h3>
           <div className='flex space-x-2 mt-[1rem]'>
             <Link to='https://www.linkedin.com/in/jesicabilotti/' target='_blank'><FaLinkedin className='w-[44px] h-[44px]' /></Link>
@@ -21,8 +21,8 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className='flex justify-center items-end m-0 h-[30%]'>
-        <p className='copyright mt-auto'>©cxperiment | Todos los derechos reservados</p>
+      <div className='mx-auto py-4 px-5'>
+        <p className='copyright text-center'>©cxperiment | Todos los derechos reservados</p>
       </div>
     </div>
   )
