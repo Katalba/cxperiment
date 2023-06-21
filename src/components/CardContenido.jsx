6ca4c07fe10847a9fb961b1c1fe8d4f4eea20215
guille <guillermoneculqueo@gmail.com>
2023-06-21 03:49:29 -0300

    se añade el difuminado

--- src/components/CardContenido.jsx
+++ src/components/CardContenido.jsx
@@ -23,10 +23,10 @@ const CardContenido = ({ item, imgBg }) => {
 
   Tamaños fuentes componente CardContenido
   titulo:
-  font-size: 20px; >= xl = 32px
-  font-size: 20px  = lg = 17px  (16.51)
-  font-size: 16px; = md = 16px
-  font-size: 16px; <= sm = 20px
+  font-size: 32px; >= xl = 1280px
+  font-size: 17px  = lg = = 1024px
+  font-size: 17px; = md = md = 768px
+  font-size: 20px; <= sm = 640px
 
   parrafo:
             tamaño  |  en resolucion:
@@ -37,15 +37,33 @@ const CardContenido = ({ item, imgBg }) => {
 
   leading-[18px] lg:leading-[24px] //linehe height
   */
+  const overlayStyleRight = {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    background: 'linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))'
+  }
+  const overlayStyleLeft = {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    background: 'linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))'
+  }
 
   return (
     <>
-      <section className={`contenedoCard card-scroll bg-whiteWi w-full h-auto lg:h-auto flex flex-col-reverse  flex-wrap justify-start items-center lg:flex-row lg:flex-nowrap lg:justify-start lg:items-center ${isPar(item.id) ? 'lg:flex-row-reverse' : 'lg:flex-row'} py-1 overflow-hidden gap-5 lg:gap-5 `}>
+      <section className={`contenedoCard card-scroll bg-whiteWi w-full h-auto md:h-auto flex flex-col-reverse  flex-wrap justify-start items-center md:flex-row md:flex-nowrap md:justify-start md:items-center ${isPar(item.id) ? 'md:flex-row-reverse' : 'md:flex-row'} py-1 overflow-hidden gap-5 lg:gap-5 `}>
 
         {/* seccion contenido */}
-        <section className='contenido w-full md:h-auto xl:h-[500px] lg:w-[40%] lg:h-full flex flex-col md:flex-row lg:flex-col justify-center items-start gap-5 md:gap-5 lg:gap-6 relative'>
-          <p className='w-full md:w-[50%] lg:w-full font-parrafo font-[400] text-textPurple text-xs lg:text-sm xl:text-base md:pb-[100px] lg:pb-0 leading-[18px] lg:leading-[24px] xl:leading-[27px]'>En <span className='font-[600]'>CXperiment</span>, {item.parrafo}</p>
-          <div className='main w-full md:w-[50%] gap-3 lg:w-full flex flex-row justify-around items-center relative'>
+        <section className='contenido w-full xl:h-[500px] md:w-[50%] lg:w-[40%] md:h-full flex flex-col md:flex-col justify-center items-start gap-5 md:gap-5 lg:gap-6 relative'>
+          <p className='w-full md:w-full font-parrafo font-[400] text-textPurple text-xs md:text-sm xl:text-base md:pb-0 leading-[18px] md:leading-[24px] xl:leading-[27px]'>En <span className='font-[600]'>CXperiment</span>, {item.parrafo}</p>
+          <div className='main w-full gap-3 md:w-full flex flex-row justify-around items-center relative'>
             <ul className='w-full flex flex-col justify-start items-start gap-3'>
               {
             item.lista.map((element) => {
@@ -65,15 +83,19 @@ const CardContenido = ({ item, imgBg }) => {
 
             </ul>
           </div>
-          <section className='pie w-full h-auto relative flex justify-start items-start md:absolute md:right-0 md:bottom-3 lg:right-auto lg:bottom-auto lg:relative lg:w-full  lg:flex-row lg:justify-start lg:items-center'>
+          <section className='pie w-full h-auto relative flex justify-start items-start md:right-auto md:bottom-auto md:relative md:w-full  md:flex-row md:justify-start md:items-center'>
             <BotonBase name='Solicita información' clase='botonVerde' />
           </section>
         </section>
 
         {/* seccion imagen */}
-        <section className={`imgBg w-full h-[90px] md:h-[280px] lg:w-[60%] lg:h-full overflow-hidden flex justify-start items-center ${isPar(item.id) ? 'justify-end' : 'justify-start'}`}>
-          <img src={imgBg} alt='logo cxPeriment' className={`h-full w-full hidden sm:block object-contain md:object-cover object-center lg:object-left opacity-40 transform relative  ${isPar(item.id) ? '-scale-x-100' : ''}`} />
-          <h2 className={`absolute w-full  text-left md:text-center lg:w-[300px] ${isPar(item.id) && 'left-2 md:left-auto'} ${isPar(item.id) ? 'lg:mr-[132px]' : 'lg:ml-[125px]'} font-titulo font-[700] text-3xl md:text-[45px] lg:text-[56px] text-textPurple leading-none`}>
+        <section className={`imgBg w-full h-auto py-2 sm:h-[50px] md:w-[50%] md:h-full lg:w-[60%] overflow-hidden flex justify-start items-center ${isPar(item.id) ? 'justify-end' : 'justify-start'} relative`}>
+          <div style={isPar(item.id) ? overlayStyleLeft : overlayStyleRight} className='absolute top-0 bottom-0 left-0 right-0 w-full h-full z-10' />
+          <img
+            src={imgBg} alt='logo cxPeriment'
+            className={`bgCustomerCards h-full w-full hidden sm:block object-contain md:object-cover object-center lg:object-left opacity-40 transform relative  ${isPar(item.id) ? '-scale-x-100' : ''}`}
+          />
+          <h2 className={`absolute w-full h-auto text-center md:w-[100px] lg:w-[130px] xl:w-[200px] ${isPar(item.id) ? 'md:mr-[50px] lg:mr-[90px] xl:mr-[70px] 2xl:mr-[120px]' : 'md:ml-[65px] lg:ml-[90px] xl:ml-[70px] 2xl:ml-[120px]'} font-titulo font-[700] text-[20px] md:text-[17px] lg:text-[17px] xl:text-[31px] 2xl:text-4xl text-textPurple leading-none`}>
             {item.titulo}
           </h2>
         </section>
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Titulo = ({ titulo }) => {
   return (
     <div className='titulo w-full text-center p-1 overflow-hidden'>
-      <h2 className='font-titulo font-[700] text-purpleBal text-center text-2xl md:text-[25px] lg:text-3xl xl:text-5xl'>{titulo}</h2>
+      <h2 className='font-titulo font-[700] text-purpleBal text-center text-2xl md:text-[25px] lg:text-3xl xl:text-[40px]'>{titulo}</h2>
     </div>
   )
 }
