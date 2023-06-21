f5ae06508e8fd60486d1734916b712da190efdff
guille <guillermoneculqueo@gmail.com>
2023-06-20 23:20:51 -0300

    re haciendo los estilos segun lo discutido hoy

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 const Titulo = ({ titulo }) => {
   return (
     <div className='titulo w-full text-center p-1 overflow-hidden'>
-      <h2 className='font-titulo font-[700] text-purpleBal text-center text-3xl md:text-4xl lg:text-5xl'>{titulo}</h2>
+      <h2 className='font-titulo font-[700] text-purpleBal text-center text-2xl md:text-[25px] lg:text-3xl xl:text-5xl'>{titulo}</h2>
     </div>
   )
 }
 
 export default Titulo
+
+/*
+Tamaños fuentes componente titulo
+font-size: 48px; >= xl = 1280px
+font-size: 30px  = lg = 1024px
+font-size: 25px; = md = 768px
+font-size: 24px; <= sm = 640px
+
+*/
