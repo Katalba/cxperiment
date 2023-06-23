cb60cc64cf13ac7e61cdb97ad0fb8a376a8f6fcb
katalba <katherine.albanicg@gmail.com>
2023-06-22 23:34:27 -0300

    flipResponsive

--- src/components/FlipCardComp.jsx
+++ src/components/FlipCardComp.jsx
@@ -17,9 +17,9 @@ const FlipCardComp = () => {
     {
       titulo: 'Customer Research',
       bgColor: 'purpleBal',
-      radiusFront: 'rounded-tr-[106px] rounded-l-[106px] rounded-t-[106px]',
-      radiusBack: 'rounded-t-[106px] rounded-br-[106px]',
-      radiusImg: 'rounded-t-[106px] rounded-br-[106px]',
+      radiusFront: 'rounded-tr-[50px] rounded-l-[50px] rounded-t-[50px] lg:rounded-tr-[106px] lg:rounded-l-[106px] lg:rounded-t-[106px]',
+      radiusBack: 'rounded-t-[50px] rounded-br-[50px] lg:rounded-t-[106px] lg:rounded-br-[106px]',
+      radiusImg: 'rounded-t-[50px] rounded-br-[50px] lg:rounded-t-[106px] lg:rounded-br-[106px]',
       textFront: 'Conociendo realmente a tu cliente',
       textBack: 'Conoce a tus clientes desde una perspectiva profunda y significativa',
       fcbImg: 'https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/custumerResearch.png?alt=media&token=bdd5bb54-1d04-4be3-b6f9-c43b04c971cd'
@@ -27,8 +27,8 @@ const FlipCardComp = () => {
     {
       titulo: 'Customer Analysis',
       bgColor: 'verdeTurk',
-      radiusFront: 'rounded-t-[106px] rounded-tr-[106px] rounded-b-[0]',
-      radiusBack: 'rounded-t-[106px] rounded-tr-[106px] rounded-b-[0]',
+      radiusFront: 'rounded-t-[50px] rounded-tr-[50px] rounded-b-[0] lg:rounded-t-[106px] lg:rounded-tr-[106px] lg:rounded-b-[0]',
+      radiusBack: 'rounded-t-[50px] rounded-tr-[50px] rounded-b-[0] lg:rounded-t-[106px] lg:rounded-tr-[106px] lg:rounded-b-[0]',
       textFront: 'Entiende a tu cliente en profundidad',
       textBack: 'Comprende a fondo a tu cliente y su experiencia con tu marca.',
       fcbImg: 'https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/customerAnalysis.png?alt=media&token=3e049d7e-c53d-4d21-a94f-7b568163f3e0'
@@ -36,9 +36,9 @@ const FlipCardComp = () => {
     {
       titulo: 'Customer Insights',
       bgColor: 'purpleBal',
-      radiusFront: 'rounded-t-[106px] rounded-br-[106px]',
-      radiusBack: 'rounded-tr-[106px] rounded-l-[106px] rounded-t-[106px]',
-      radiusImg: 'rounded-tr-[106px] rounded-l-[106px] rounded-t-[106px]',
+      radiusFront: 'rounded-t-[50px] rounded-br-[50px] lg:rounded-t-[106px] lg:rounded-br-[106px]',
+      radiusBack: 'rounded-tr-[50px] rounded-l-[50px] rounded-t-[50px] lg:rounded-tr-[106px] lg:rounded-l-[106px] lg:rounded-t-[106px]',
+      radiusImg: 'rounded-tr-[50px] rounded-l-[50px] rounded-t-[50px] lg:rounded-tr-[106px] lg:rounded-l-[106px] lg:rounded-t-[106px]',
       textFront: 'Fidelicemos a tu cliente',
       textBack: 'Descubre aspectos relevantes de tus clientes y sus preferencias.',
       fcbImg: 'https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/customerInsights.png?alt=media&token=440802da-e4df-4671-b064-a5bf24cc8af6'
@@ -84,7 +84,7 @@ const FlipCardComp = () => {
         <HiChevronLeft />
       </button>
       <section
-        className={`cajaPadre w-[260px] sm:w-[310px] md:w-[380px] h-auto overflow-hidden lg:w-full flex ${calculaFlex(currentCardIndex)} lg:justify-center gap-4 pt-10 py-20 lg:py-5`}
+        className={`cajaPadre w-[190px] min-[450px]:w-[250px] sm:w-[310px] md:w-full  h-auto overflow-hidden lg:w-full flex ${calculaFlex(currentCardIndex)} md: justify-center lg:justify-center gap-4 pt-10 py-20 lg:py-5`}
         // style={{ transform: `translateX(${getTranslateX()}px)` }}
       >
         {
