5d2a610d9bdb0fb53c1205ddada253662a42ad6d
katalba <katherine.albanicg@gmail.com>
2023-06-23 14:26:55 -0300

    quoteModificado

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -6,26 +6,25 @@ const QuoteSection = () => {
   const { sobremiRef } = useGeneralContext()
 
   return (
-    <section ref={sobremiRef} id='sobremi' className='card-scroll contenedor w-full h-[calc(100vh_-_90px)] bg-verdeTurk relative flex justify-center items-center space-y-[2rem] overflow-hidden'>
-      <section className='container absolute h-full flex flex-col justify-center items-center space-y-6'>
+    <section ref={sobremiRef} id='sobremi' className='card-scroll w-full h-[calc(100vh_-_90px)] bg-verdeTurk flex justify-center items-center'>
+      <section className='w-full h-auto flex flex-col items-center justify-center space-y-6'>
         <div className='logo w-[240px] h-[31px]'>
           <img className='w-full h-full' src='https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/logoWhite.png?alt=media&token=e025e897-8318-46c4-b001-3ad03397f262' alt='logo' />
         </div>
-        <div className='quote w-full px-20 text-center '>
-          <p className='w-full text-whiteWi font-parrafo font-[400] text-base sm:text-base md:text-lg lg:text-xl xl:text-xl text-center'> “Mi objetivo es brindar a las empresas las herramientas necesarias para alcanzar el éxito a través de la mejora continua de su Customer Experience”.</p>
+        <div className='quote w-full px-10 md:px-40 text-center '>
+          <p className='w-full text-whiteWi font-parrafo font-[400] text-base sm:text-base md:text-lg lg:text-[30px] xl:text-[30px] text-center'> “Mi objetivo es brindar a las empresas las herramientas necesarias para alcanzar el éxito a través de la mejora continua de su Customer Experience”.</p>
         </div>
         <div className='w-[120px] h-[120px] md:w-[150px] md:h-[150px] lg:w-[160px] lg:h-[160px]'>
           <img className='object-contain object-center rounded-full grayscale' src='https://firebasestorage.googleapis.com/v0/b/cxperiment.appspot.com/o/Jesica.svg?alt=media&token=1e6a32bf-50bc-4b87-bedf-0ca751429aa1' alt='founder' />
         </div>
-        <div className='w-[200px] h-[52px] text-center'>
+        <div className='w-full h-auto text-center'>
           <h3 className=' text-whiteWi text-xs sm:text-xs md:text-xs lg:text-sm xl:text-base font-semibold'> Jesica Bilotti</h3>
           <p className='text-whiteWi text-xs sm:text-xs md:text-xs lg:text-sm xl:text-base'> Customer Experience</p>
         </div>
-        <div className=' flex justify-center w-[200px] h-[52px] space-x-[1rem]'>
+        <div className=' flex space-x-[1rem]'>
           <Link to='https://www.linkedin.com/in/jesicabilotti/' target='_blank'><FaLinkedin className='invert-[100%] w-[2rem] h-[2rem] transition duration-500 ease-in-out hover:scale-125 hover:text-purpleBal hover:invert-[0]' /></Link>
           <Link to='https://www.instagram.com/cxperiment/' target='_blank'><FaInstagram className='invert-[100%] w-[2rem] h-[2rem] transition duration-500 ease-in-out hover:scale-125 hover:text-purpleBal hover:invert-[0]' /> </Link>
         </div>
-        <div />
       </section>
     </section>
   )
