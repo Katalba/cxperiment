277769aa8abec86435d5068aeb8a5d4c30c379c4
katalba <katherine.albanicg@gmail.com>
2023-06-22 21:54:05 -0300

    probandoBanner

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -13,13 +13,13 @@ const FlipCard = ({ objeto }) => {
         <div className={`cardFront ${cardBg} absolute ${radiusFront} w-full h-full [backface-visibility: visible] flex flex-col justify-center items-center p-[13%]`}>
           <div className='w-full h-full p-2 flex flex-col justify-center items-center gap-4 overflow-hidden'>
             <Title name={`${titulo}`} />
-            <p className='text-lg text-whiteWi text-center'>{textFront}</p>
+            <p className='text-xs sm:text-xs md:text-xs lg:text-sm xl:text-base text-whiteWi text-center'>{textFront}</p>
           </div>
         </div>
         <div className={`cardBack ${cardBg} absolute ${radiusBack} w-full h-full [transform:rotateY(180deg)] [backface-visibility:hidden] flex flex-col justify-center items-center overflow-hidden`}>
           <img className={`absolute w-full h-full object-fill ${radiusImg}`} src={`${fcbImg}`} alt='imagenServicio' />
           <div className='w-full h-full absolute text-center overflow-hidden p-[15%] flex flex-col justify-center items-center gap-4'>
-            <p className='text-lg text-whiteWi font-semibold'>{textBack}</p>
+            <p className='text-xs sm:text-xs md:text-xs lg:text-sm xl:text-base text-whiteWi font-semibold'>{textBack}</p>
             <BotonBase name='Más info' clase='botonVerde' />
           </div>
         </div>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,12 +4,12 @@ const Title = ({ name }) => {
   // Tamaños fuentes FlipCard
   // titulo:
   // font-size: 40px; >= xl = 1280px
-  // font-size: 43px  = lg = 1024px
-  // font-size: 31px; = md = 768px
-  // font-size: 31px; <= sm = 640px
+  // font-size: 38px  = lg = 1024px
+  // font-size: 38px; = md = 768px
+  // font-size: 29px; <= sm = 640px
   return (
     <>
-      <h2 className='text-center text-whiteWi font-titulo font-bold text-[31px] md:text-[31px] lg:text-[40px] xl:text-[43px]'>{name}</h2>
+      <h2 className='text-center leading-tight text-whiteWi font-titulo font-bold text-[29px] md:text-[38px] lg:text-[38px] xl:text-[43px]'>{name}</h2>
     </>
   )
 }
