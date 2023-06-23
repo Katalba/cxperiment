8f1b5650545db7c7ca4e24471e4ad07d96acb500
katalba <katherine.albanicg@gmail.com>
2023-06-23 12:41:58 -0300

    fcardResponsive

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -8,7 +8,7 @@ const FlipCard = ({ objeto }) => {
   const cardBg = `bg-${bgColor}`
 
   return (
-    <section className='flipCard group w-[190px] h-[190px] min-[450px]:w-[250px] min-[450px]:h-[250px] sm:w-[310px] sm:h-[310px] md:w-[200px] md:h-[200px] lg:w-[310px] lg:h-[310px] xl:w-[350px] xl:h-[350px] aspect-square p-0 '>
+    <section className='flipCard group w-[190px] h-[190px] min-[450px]:w-[250px] min-[450px]:h-[250px] sm:w-[310px] sm:h-[310px] md:w-[200px] md:h-[200px] lg:w-[298px] lg:h-[298px] xl:w-[350px] xl:h-[350px] aspect-square p-0 '>
       <div className='cardService relative w-full h-full transition-all ease-in-out duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]'>
         <div className={`cardFront ${cardBg} absolute ${radiusFront} w-full h-full [backface-visibility: visible] flex flex-col justify-center items-center p-[13%]`}>
           <div className='w-full h-full p-2 flex flex-col justify-center items-center gap-4 overflow-hidden'>
--- src/components/FlipCardComp.jsx
+++ src/components/FlipCardComp.jsx
@@ -80,11 +80,11 @@ const FlipCardComp = () => {
       id='servicios'
       className='w-full h-auto flex justify-center items-center gap-2 relative card-scroll'
     >
-      <button className='block lg:hidden text-[30px] text-verdeTurk ' onClick={handleClickLeft}>
+      <button className='block md:hidden text-[30px] text-verdeTurk ' onClick={handleClickLeft}>
         <HiChevronLeft />
       </button>
       <section
-        className={`cajaPadre w-[190px] min-[450px]:w-[250px] sm:w-[310px] md:w-full  h-auto overflow-hidden lg:w-full flex ${calculaFlex(currentCardIndex)} md: justify-center lg:justify-center gap-4 pt-10 py-20 lg:py-5`}
+        className={`cajaPadre w-[190px] min-[450px]:w-[250px] sm:w-[310px] h-auto overflow-hidden md:w-full lg:w-full flex ${calculaFlex(currentCardIndex)} md:justify-center lg:justify-center gap-4 pt-10 py-20 lg:py-5`}
         // style={{ transform: `translateX(${getTranslateX()}px)` }}
       >
         {
@@ -100,10 +100,10 @@ const FlipCardComp = () => {
           })
         }
       </section>
-      <button className='block lg:hidden text-[30px] text-verdeTurk ' onClick={handleClickRight}>
+      <button className='block md:hidden text-[30px] text-verdeTurk ' onClick={handleClickRight}>
         <HiChevronRight />
       </button>
-      <div className='w-full h-[30px] absolute bottom-10 left-0 flex justify-center items-center gap-5 lg:hidden'>
+      <div className='w-full h-[30px] absolute bottom-10 left-0 flex justify-center items-center gap-5 md:hidden'>
         <span className={`h-[12px] aspect-square ${currentCardIndex === 2 ? 'bg-verdeTurk' : 'bg-purpleBal'} rounded-full`} />
         <span className={`h-[12px] aspect-square rounded-full ${currentCardIndex === 0 ? 'bg-verdeTurk' : 'bg-purpleBal'}`} />
         <span className={`h-[12px] aspect-square rounded-full ${currentCardIndex === 1 ? 'bg-verdeTurk' : 'bg-purpleBal'}`} />
