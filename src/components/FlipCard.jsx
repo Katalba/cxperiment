7b1caea96d50c04dd4e847d36900b72ecc4cfd5a
katalba <katherine.albanicg@gmail.com>
2023-06-23 22:00:49 -0300

    clickCard

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Title from './Title'
 import BotonBase from './BotonBase'
 
 const FlipCard = ({ objeto }) => {
+  const [isFlipped, setIsFlipped] = useState(false)
   const { bgColor, radiusFront, radiusBack, radiusImg, textFront, textBack, fcbImg, titulo } = objeto
 
   const cardBg = `bg-${bgColor}`
 
-  const [isFlipped, setIsFlipped] = useState(false)
-
-  useEffect(() => {
-    function rotateCard () {
-      if (window.innerWidth <= 780) {
-        setIsFlipped(prevState => !prevState)
-      }
-    }
-
-    const cardElement = document.getElementById('flipCard')
-    cardElement.addEventListener('click', rotateCard)
-
-    return () => {
-      cardElement.removeEventListener('click', rotateCard)
-    }
-  }, [isFlipped])
+  const flipCard = () => {
+    setIsFlipped(prevState => !prevState)
+  }
 
   return (
-    <section className='flipCard group w-[190px] h-[190px] min-[450px]:w-[250px] min-[450px]:h-[250px] sm:w-[310px] sm:h-[310px] md:w-[200px] md:h-[200px] lg:w-[298px] lg:h-[298px] xl:w-[350px] xl:h-[350px] aspect-square p-0'>
-      <div id='flipCard' className={`cardService relative w-full h-full transition-all ease-in-out duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] ${isFlipped ? '[transform:rotateY(180deg)]' : ''} `}>
+    <section className='flipCard group cursor-pointer w-[190px] h-[190px] min-[450px]:w-[250px] min-[450px]:h-[250px] sm:w-[310px] sm:h-[310px] md:w-[200px] md:h-[200px] lg:w-[298px] lg:h-[298px] xl:w-[350px] xl:h-[350px] aspect-square p-0' onClick={flipCard}>
+      <div id='flipCard' className={`cardService relative w-full h-full transition-all ease-in-out duration-700 [transform-style:preserve-3d] ${isFlipped ? '[transform:rotateY(180deg)]' : '[transform:rotateY(0deg)]'} `}>
         <div className={`cardFront ${cardBg} absolute ${radiusFront} w-full h-full [backface-visibility: visible] flex flex-col justify-center items-center p-[13%]`}>
           <div className='w-full h-full p-2 flex flex-col justify-center items-center gap-4 overflow-hidden'>
             <Title name={`${titulo}`} />
