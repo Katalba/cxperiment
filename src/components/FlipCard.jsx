46c0ffa8a46a489eade022d1bb327491fa165614
katalba <katherine.albanicg@gmail.com>
2023-06-25 00:43:31 -0300

    flipCardMoreBig

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -13,7 +13,7 @@ const FlipCard = ({ objeto }) => {
   }
 
   return (
-    <section className='flipCard group cursor-pointer w-[190px] h-[190px] min-[450px]:w-[250px] min-[450px]:h-[250px] sm:w-[310px] sm:h-[310px] md:w-[200px] md:h-[200px] lg:w-[298px] lg:h-[298px] xl:w-[350px] xl:h-[350px] aspect-square p-0' onClick={flipCard} onMouseEnter={flipCard} onMouseLeave={flipCard}>
+    <section className='flipCard group cursor-pointer w-[250.627px] h-[250.627px] md:w-[200px] md:h-[200px] lg:w-[298px] lg:h-[298px] xl:w-[381px] xl:h-[381px] aspect-square p-0' onClick={flipCard} onMouseEnter={flipCard} onMouseLeave={flipCard}>
       <div id='flipCard' className={`cardService relative w-full h-full transition-all ease-in-out duration-700 [transform-style:preserve-3d] ${isFlipped ? '[transform:rotateY(180deg)]' : '[transform:rotateY(0deg)]'} `}>
         <div className={`cardFront ${cardBg} absolute ${radiusFront} w-full h-full [backface-visibility: visible] flex flex-col justify-center items-center p-[13%]`}>
           <div className='w-full h-full p-2 flex flex-col justify-center items-center gap-4 overflow-hidden'>
